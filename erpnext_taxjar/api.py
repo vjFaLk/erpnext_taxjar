b439bc018850fb763fe84f92c87af0d830ee8dde
Valmik Jangla <mail@valmik.in>
2017-06-30 20:43:22 +0530

    [fixes] multiple fixes

--- erpnext_taxjar/api.py
+++ erpnext_taxjar/api.py
@@ -6,25 +6,30 @@ import taxjar
 
 
 def get_client():
-	taxjar_settings = frappe.get_single("TaxJar Settings")	
+	taxjar_settings = frappe.get_single("TaxJar Settings")
 	client = taxjar.Client(api_key=taxjar_settings.get_password("api_key"))
 	return client
+
 
 def set_sales_tax(doc, method):
 	if not doc.items:
 		return
 
-	taxjar_settings = frappe.get_single("TaxJar Settings")	
-		
+	taxjar_settings = frappe.get_single("TaxJar Settings")
+
 	client = get_client()
 	tax_dict = get_tax_data(doc)
+
+	if not tax_dict:
+		return
+
 	taxdata = client.tax_for_order(tax_dict)
 
 	if "Sales Tax" in [tax.description for tax in doc.taxes]:
-				for tax in doc.taxes:
-					if tax.description == "Sales Tax":
-						tax.tax_amount = taxdata.amount_to_collect
-						break
+		for tax in doc.taxes:
+			if tax.description == "Sales Tax":
+				tax.tax_amount = taxdata.amount_to_collect
+				break
 	elif taxdata.amount_to_collect > 0:
 		doc.append("taxes", {
 			"charge_type": "Actual",
@@ -35,6 +40,7 @@ def set_sales_tax(doc, method):
 
 		doc.run_method("calculate_taxes_and_totals")
 
+
 def create_transaction(doc, method):
 
 	client = get_client()
@@ -44,17 +50,17 @@ def create_transaction(doc, method):
 			sales_tax = tax.tax_amount
 
 	if not sales_tax:
-		return 
+		return
 
 	tax_dict = get_tax_data(doc)
 	if not tax_dict:
-		return 
+		return
 
 	tax_dict['transaction_id'] = doc.name
 	tax_dict['transaction_date'] = frappe.utils.today()
 	tax_dict['sales_tax'] = sales_tax
-	tax_dict['amount'] = doc.grand_total - sales_tax
-	
+	tax_dict['amount'] = doc.grand_total - sales_tax	
+
 	order = client.create_order(tax_dict)
 
 def delete_transaction(doc, method):
@@ -63,7 +69,7 @@ def delete_transaction(doc, method):
 
 
 def get_tax_data(doc):
-	taxjar_settings = frappe.get_single("TaxJar Settings")	
+	taxjar_settings = frappe.get_single("TaxJar Settings")
 
 	client = get_client()
 	if not (taxjar_settings.api_key or taxjar_settings.tax_account_head):
@@ -86,26 +92,25 @@ def get_tax_data(doc):
 			shipping = tax.tax_amount
 
 	tax_dict = {
-			'to_country': 'US',
-			'to_zip': shipping_address.pincode,
-			'to_city': shipping_address.city,
-			'to_state': shipping_address.state,
-			'from_country': 'US',
-			'from_zip': company_address.pincode,
-			'from_city': company_address.city,
-			'shipping': shipping,
-			'amount' : doc.total,
-			'line_items': []
-		}
+		'to_country': 'US',
+		'to_zip': shipping_address.pincode,
+		'to_city': shipping_address.city,
+		'to_state': shipping_address.state,
+		'from_country': 'US',
+		'from_zip': company_address.pincode,
+		'from_city': company_address.city,
+		'shipping': shipping,
+		'line_items': []
+	}
 
 	index = 1
 	for item in doc.items:
 		tax_dict["line_items"].append({
-			"id" : index,
-			"description" : item.item_name,
+			"id": index,
+			"description": item.item_name,
 			"unit_price": item.rate,
 			"quantity": item.qty,
-			"discount" : (item.discount_percentage / 100) * item.price_list_rate
+			"discount": (item.discount_percentage / 100) * item.price_list_rate
 		})
 
 		index = index + 1
